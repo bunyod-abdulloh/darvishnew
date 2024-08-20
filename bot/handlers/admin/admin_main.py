c955bceaa311fcdc2b894417dc224c6544a1cbb6
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-08-21 03:47:12 +0500

    Updated

--- bot/handlers/admin/admin_main.py
+++ bot/handlers/admin/admin_main.py
@@ -1,8 +1,10 @@
 from aiogram import Router, types, F
 from aiogram.filters import Command
-from data.config import ADMINS
+
 from bot.filters import IsBotAdminFilter
 from bot.keyboards.reply.admin_dkb import admin_main_dkb
+from darvishweb.darvishweb.settings import A_HOSTS
+from data.config import ADMINS
 
 router = Router()
 
@@ -17,7 +19,7 @@ async def admin_main_router(message: types.Message):
 @router.message(IsBotAdminFilter(ADMINS), F.text == "Web adminka")
 async def admin_media_main_router(message: types.Message):
     await message.answer(
-        text=f"<a href='http://89.23.115.249/admin/'>Admin web sahifasiga o'tish</a>"
+        text=f"<a href='http://{A_HOSTS}/admin/'>Admin web sahifasiga o'tish</a>"
     )
 
 
