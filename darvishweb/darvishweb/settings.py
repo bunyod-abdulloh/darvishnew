5f0a538eb265dfa5708560bb741cded2ee36d1e7
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-09-27 10:50:30 +0500

    Updated

--- darvishweb/darvishweb/settings.py
+++ darvishweb/darvishweb/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = '/root/darvishnew/darvishweb/static/'
+# STATIC_ROOT = '/root/darvishnew/darvishweb/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
