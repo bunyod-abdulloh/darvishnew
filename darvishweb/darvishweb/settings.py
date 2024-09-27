b424b3c4e3356faa67280f39571eeb412b8d381e
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-09-27 10:57:00 +0500

    Updated

--- darvishweb/darvishweb/settings.py
+++ darvishweb/darvishweb/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from environs import Env
@@ -117,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = '/root/darvishnew/darvishweb/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
