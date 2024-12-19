bf05b3d40b851232c9b74d36fcfa82abc0844c2e
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2024-12-19 07:49:56 +0500

    Updated

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -7,6 +7,6 @@ async def on_startup_notify(bot: Bot):
     try:
         await bot.send_message(
             chat_id=-1001399496245,
-            text="Bot ishga tushdi")
+            text="Bot ishga tushdi!\n\nGavharDarvishBot")
     except Exception as err:
         logging.exception(err)
